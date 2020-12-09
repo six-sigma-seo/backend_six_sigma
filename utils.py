992a200531f15dd34e02625724a8d64496764593
franco123 <58991258+juanfranco97@users.noreply.github.com>
2020-12-08 23:26:57 -0600

    add new funcion to get scrapy data on utils file

--- utils.py
+++ utils.py
@@ -1,8 +1,23 @@
 """ Some utilities that are used in the application"""
-import pandas as pd
+
+#Python
+import requests
+import json
+
 
 class Utils:
 
-    def load_from_csv(self, path):
-        """ Read a csv """
-        return pd.read_csv(path)
+    def get_scrapi_data(web_site):
+        """ this funcion resive a web_site and return a diccionary 
+            with information about the web site based on a scrapy sistem. """
+        r = requests.get('https://app.scrapinghub.com/api/v2/datasets/UhNGprd2Cts/download?format=json') 
+        if r.status_code == 200:
+            data1 = r.text 
+            data2 = r.text
+            data1 = data1[1:]
+            data1 = data1[:-1]
+            data1 = json.loads(data1)
+        else: 
+            data1 = 0
+            data2 = 0
+        return data1, data2
